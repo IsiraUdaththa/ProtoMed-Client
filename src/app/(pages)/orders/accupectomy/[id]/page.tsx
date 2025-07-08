a3fc5e920108a9db79e774ad25249062e795e784
Isira <isira.udattha@gmail.com>
2025-07-08 15:48:03 +0530

    remove Order type specific pages

--- src/app/(pages)/orders/accupectomy/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useParams } from "next/navigation";
-
-export default function OrderDetailsPage() {
-	const params = useParams();
-	return <h1>Viewing Order {params.id}</h1>;
-}
